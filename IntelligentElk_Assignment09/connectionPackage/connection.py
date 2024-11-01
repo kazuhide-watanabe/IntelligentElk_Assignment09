1a7267857f0c5e06f2441abb845e7739acd871a3
kazuhide-watanabe <watanake@mail.uc.edu>
2024-11-01 13:14:27 -0400

    Connected to the database for the assignment, and added documentation to the Connection.py

--- IntelligentElk_Assignment09/connectionPackage/connection.py
+++ IntelligentElk_Assignment09/connectionPackage/connection.py
@@ -1,19 +1,35 @@
+#*********************************
+# Name: Kazuhide Watanabe, Alex Carnes, Alex Patton
+# email:  watanake@mail.uc.edu
+#         carnesas@mail.uc.edu
+#         pattona6@mail.uc.edu
+# Assignment Number: Assignment 09
+# Due Date:  11/07/2024
+# Course #/Section: IS 4010/001 
+# Semester/Year:   Fall/2024
+# Brief Description of the assignment: 
+# Brief Description of what this module does:
+# Citations: In class notes, Bill Nicholson 
+# Anything else that's relevant: 
+#**********************************
+
 # Connection.py
 
 import pyodbc
 
 def Connect():
     """
-    Connect to the database
-    @return Connection Object: The open connection, or None on error
+    Coneect to the database
+    @return Connection Object: The open connection or none, or error
     """
     try:
         conn = pyodbc.connect('Driver={SQL Server};'
-                              'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
-                              'Database=IS4010;'
-                              'uid=IS4010Login;'
-                              'pwd=P@ssword2;')
-    except:
+                                  'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+                                  'Database=GroceryStoreSimulator;'
+                                  'uid=IS4010Login;'
+                                  'pwd=P@ssword2;')
+    except :
         conn = None
-    
+
     return conn
+
--- IntelligentElk_Assignment09/mainPackage/main.py
+++ IntelligentElk_Assignment09/mainPackage/main.py
@@ -3,15 +3,14 @@
 # email:  watanake@mail.uc.edu
 #         carnesas@mail.uc.edu
 #         pattona6@mail.uc.edu
-# Assignment Number: Assignment 08
-# Due Date:  10/31/2024
+# Assignment Number: Assignment 09
+# Due Date:  11/07/2024
 # Course #/Section: IS 4010/001 
 # Semester/Year:   Fall/2024
-# Brief Description of the assignment: Group project where each member creates a class to be called to the main module.
-# Brief Description of what this module does: This module imports the Stove and Fridge file. 
-# It then creates and instance of the classes, invokes the non-dunder and dunder methods testing normal and exception cases of using the class. 
+# Brief Description of the assignment: 
+# Brief Description of what this module does: 
 # Citations: In class notes, Bill Nicholson 
-# Anything else that's relevant: This python file was assigned to Kazuhide Watanabe
+# Anything else that's relevant: 
 #**********************************
 
 # main.py
@@ -21,7 +20,6 @@ from connectionPackage.Connection import *
 
 try:
     conn = Connect()
-
     # Submit a query to the SQL Server instance and store the results in the cursor object 
     cursor = conn.cursor()
 
